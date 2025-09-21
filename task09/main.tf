abc6cc913d2c634e41ab4722a3f55a1de1ff2163
Sümeyye Ünal <90193242+sumpertramp@users.noreply.github.com>
2025-09-21 11:46:10 +0300

    Add files via upload

--- task09/main.tf
+++ task09/main.tf
@@ -1,36 +1,29 @@
-data "azurerm_resource_group" "rg" {
-  name = var.rg_name
-}
-
-data "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
 resource "azurerm_subnet" "fw_subnet" {
-  name                 = var.fw_subnet_name # AzureFirewallSubnet olmalı
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  name                 = var.fw_subnet_name         # "AzureFirewallSubnet"
+  resource_group_name  = var.rg_name                # << data.* yerine var.*
+  virtual_network_name = var.vnet_name              # << data.* yerine var.*
   address_prefixes     = [var.fw_subnet_address_prefix]
 }
 
 module "firewall" {
-  source = "./modules/firewall"
+  source            = "./modules/firewall"
 
-  rg_name  = data.azurerm_resource_group.rg.name
-  location = var.location
+  rg_name           = var.rg_name
+  location          = var.location
 
-  vnet_name    = data.azurerm_virtual_network.vnet.name
-  fw_subnet_id = azurerm_subnet.fw_subnet.id # ← data.* yerine resource id
+  # vnet_name modülde zorunlu değilse göndermesen de olur; gönderiyorsan var'dan gönder:
+  vnet_name         = var.vnet_name
 
-  fw_name     = var.fw_name
-  fw_pip_name = var.fw_pip_name
+  fw_subnet_id      = azurerm_subnet.fw_subnet.id
+
+  fw_name           = var.fw_name
+  fw_pip_name       = var.fw_pip_name
 
   aks_loadbalancer_ip = var.aks_loadbalancer_ip
   nat_app_ports       = var.nat_app_ports
+  aks_node_subnet_id  = var.aks_node_subnet_id
 
-  aks_node_subnet_id = var.aks_node_subnet_id
-  tags               = var.tags
+  tags = var.tags
 }
 
 
