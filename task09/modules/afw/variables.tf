931e7a42e5aa611f7b16092fee787300d6ee739b
Sümeyye Ünal <90193242+sumpertramp@users.noreply.github.com>
2025-09-22 04:27:11 +0300

    Add files via upload

--- task09/modules/afw/variables.tf
+++ task09/modules/afw/variables.tf
@@ -1,13 +1,37 @@
-variable "location" { type = string }
-variable "resource_group_name" { type = string }
+variable "location" {
+  description = "Azure region for all resources (e.g., East US)."
+  type        = string
+}
 
-variable "vnet_id" { type = string }
-variable "aks_subnet_id" { type = string }
+variable "resource_group_name" {
+  description = "Name of the existing Resource Group that hosts the VNet and AKS."
+  type        = string
+}
 
-variable "firewall_name" { type = string }
-variable "firewall_pip_name" { type = string }
-variable "route_table_name" { type = string }
+variable "vnet_id" {
+  description = "Resource ID of the existing Virtual Network."
+  type        = string
+}
 
+variable "aks_subnet_id" {
+  description = "Resource ID of the existing AKS subnet to associate with the route table."
+  type        = string
+}
+
+variable "firewall_name" {
+  description = "Name for the Azure Firewall resource."
+  type        = string
+}
+
+variable "firewall_pip_name" {
+  description = "Name for the Public IP address assigned to the Azure Firewall."
+  type        = string
+}
+
+variable "route_table_name" {
+  description = "Name for the route table used to force egress through the firewall."
+  type        = string
+}
 variable "fw_subnet_cidr" {
   description = "CIDR for AzureFirewallSubnet"
   type        = string
@@ -19,6 +43,7 @@ variable "aks_loadbalancer_ip" {
 }
 
 variable "tags" {
+  description = "Tag"
   type = map(string)
 }
 
